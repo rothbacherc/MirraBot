91999c94019a4d3e68ea5997fc34c1ade02cb279
c_rothbacher <crothbacher@peerassist.com>
2019-05-21 17:14:28 -0400

    Made our boy a bit object oriented

--- mirrabot.py
+++ mirrabot.py
@@ -1,158 +1,88 @@
 #Version 2.0 Dev: Charles Rothbacher
-
 import random
-import requests
-from requests.exceptions import HTTPError
 
-#there are built in functions and libraries that do similar things possibly
-#better, but this is a really simple funtion that takes only the punctuations
-#that I want out
-def textFormatter(text):
-    punct = ['!','$','%','*','(',')','-',',','.','[',']','{','}','?','@']
-    for i in punct:
-        text = text.replace(i,' ')
-    return text
+class mirrabot:
+    mLength = 0
+    mList = []
 
-def sendText(chat_id, text):
-    sendURL = baseURL 
-    sendURL += 'sendMessage'
-    PARAMS = {'chat_id': chat_id, 'text': text}
-    try:
-        requests.post(url = sendURL, params = PARAMS)
-        print("Send Worked")
-    except:
-        print("Send Broke")
+    def __init__(self):
+        outFile = open('mirraism.txt','r')
+        for strLine in outFile:
+            self.mList.append(strLine[:-1])
+            self.mLength += 1
+        outFile.close()
 
-def getPatchNotes(allNotes,chat_id):
-    outFile = open('mirrabotPatchNotes.txt','r')
-    #make a new string that will be combined with each line in the file
-    #and allow them to be printed out all as is
-    notesString = '*'
-    for line in outFile:
-        notesString = notesString + line
-    #finds the last * which denotes the beginning of a specific version
-    if allNotes == False: 
-        start = notesString.rfind('*')
-    else:
-        start = 1
-    sendText(chat_id, notesString[int(start):])      #fix here
-    outFile.close()
+    #there are built in functions and libraries that do similar things possibly
+    #better, but this is a really simple funtion that takes only the punctuations
+    #that I want out
+    def textFormatter(self, text):
+        punct = ['!','$','%','*','(',')','-',',','.','[',']','{','}','?','@']
+        for i in punct:
+            text = text.replace(i,' ')
+        return text
 
-def writeIn(chat_id,text):
-    inFile = open('mirraism.txt','a+')
-    inFile.write(text + '\n')
-    mList.append(text)
-    length = len(mList) + 1
-    inFile.close()
-    print(length)
-    #find out how long inFile is, have to open it again specifically for reading
-    #inFile = open('mirraism.txt','r')
-    #i = 0
-    #for line in inFile:
-    #    i += 1
-    if length % 100 == 0:
-        #used double quotes to get around the apostrophe
-        sendText(chat_id, "Sham bakkala! I've learned "+ str(length) +" phrases!")      #fix here
+    def sendText(self, chat_id, text):
+        return {'chat_id': chat_id, 'text': text}
 
-
-def mirraSays(chat_id):
-    #outFile = open('mirraism.txt','r')
-    #length = 0
-    #mList = []
-    #for strLine in outFile:
-    #    mList.append(strLine[:-1])
-    #    length += 1
-    ranNum = random.randint(0,mLength-1)
-    #print(length)
-    sendText(chat_id, mList[ranNum]) #bot.sendMessage(chat_id, mList[ranNum])      #fix here
-    #outFile.close()
-    
-def handleMe(msg):
-    user_name = msg['from']['username']
-    user_id = msg['from']['id']
-    text = msg['text']
-    chat_id = msg['chat']['id']
-    dice = random.randint(1,6)
-    wordTest = []
-    inFlag = False
-    outFlag = False
-    patchFlag = False
-    hasLearned = False
-    allNotes = False
-
-    #pprint.pprint(msg)
-
-    #checks if this is a reply or not to use with teaching mirrabot phrases    
-    if 'reply_to_message' in msg:
-        replyText = msg['reply_to_message']['text']
-        replyName = msg['reply_to_message']['from']['username']
-
-    replaced = textFormatter(text)
-    wordTest = replaced.split(' ')
-
-    for i in wordTest:
-        if user_name == mirra and (i == 'chicken' or i == 'apple'):
-            inFlag = True
-        elif i == '/mirrasays':
-            outFlag = True
-        
-    #if text  == '/update' and user_name == admin:       #fix here
-    #    bot.getUpdates
-    if text == '/patchnotes' and user_name == admin:
-        getPatchNotes(allNotes,chat_id)
-    elif text == '/allpatchnotes' and user_name == admin:
-        allNotes = True
-        getPatchNotes(allNotes,chat_id)
-    elif text == '/mirrasays' or outFlag == True:
-        mirraSays(chat_id)
-    elif text == '/mirralearns' and user_name != mirra and replyName == mirra:
-        inFile = open('mirraism.txt','r')
-        for strLine in inFile:
-            if strLine[:-1] == replyText:
-                hasLearned = True
+    def writeIn(self, chat_id, text):
+        inFile = open('mirraism.txt','a+')
+        inFile.write(text + '\n')
+        self.mList.append(text)
+        self.mLength += 1
         inFile.close()
-        if hasLearned == True:
-            sendText(chat_id, 'I already knew that you butthead!')               #fix here
-        else:
-            writeIn(chat_id,replyText)
-            sendText(chat_id,replyText)                                 #fix here
-    else:
-        if inFlag == True:
-            writeIn(chat_id,text)
-        elif user_name == 'mirracles' and dice > 3 and len(text) > 10:
-            writeIn(chat_id,text)
+        print(self.mLength)
 
-offset = 0
-inFile = open('botKey.txt', 'r')
-key = inFile.readline()
-admin = inFile.readline()
-mirra = inFile.readline()
-inFile.close()
+        #breaking for now
+        #if self.mLength % 100 == 0:
+            #used double quotes to get around the apostrophe
+        #    return self.sendText(chat_id, "Sham bakkala! I've learned "+ str(self.mLength) +" phrases!")      #fix here
 
-outFile = open('mirraism.txt','r')
-mLength = 0
-mList = []
-for strLine in outFile:
-    mList.append(strLine[:-1])
-    mLength += 1
-outFile.close()
 
-baseURL = 'https://api.telegram.org/bot'
-baseURL += key + '/'
-print("I'm on!")
+    def mirraSays(self, chat_id):
+        ranNum = random.randint(0,self.mLength-1)
+        return self.sendText(chat_id, self.mList[ranNum]) 
 
-while 1:
-    try:
-        result = requests.post(url = baseURL + 'getUpdates', data={"offset": offset})
-        result.raise_for_status()
-        msgList = result.json()["result"]
-        if len(msgList) > 0:
-            offset = msgList[-1]["update_id"] + 1
-            for msg in msgList:
-                print(msg)
-                handleMe(msg['message'])
-    except HTTPError as http_err:
-        print("It broke")
-    except KeyboardInterrupt as e:
-        print("End")
-        break
+    def handleMe(self, msg, admin, mirra):
+        if 'text' in msg:
+            user_name = msg['from']['username']
+            text = msg['text']
+            chat_id = msg['chat']['id']
+            dice = random.randint(1,6)
+            wordTest = []
+            inFlag = False
+            outFlag = False
+            hasLearned = False
+
+            #checks if this is a reply or not to use with teaching mirrabot phrases    
+            if 'reply_to_message' in msg:
+                replyText = msg['reply_to_message']['text']
+                replyName = msg['reply_to_message']['from']['username']
+
+            replaced = self.textFormatter(text)
+            wordTest = replaced.split(' ')
+
+            for i in wordTest:
+                if user_name == mirra and (i == 'chicken' or i == 'apple'):
+                    inFlag = True
+                elif i == '/mirrasays':
+                    outFlag = True
+
+            if text == '/mirrasays' or outFlag == True:
+                return self.mirraSays(chat_id)
+            elif text == '/mirralearns' and user_name != mirra and replyName == mirra:
+                inFile = open('mirraism.txt','r')
+                for strLine in inFile:
+                    if strLine[:-1] == replyText:
+                        hasLearned = True
+                inFile.close()
+                if hasLearned == True:
+                    return self.sendText(chat_id, 'I already knew that you butthead!')               
+                else:
+                    self.writeIn(chat_id,replyText)
+                    return self.sendText(chat_id,replyText)                                 
+            else:
+                if inFlag == True:
+                    self.writeIn(chat_id,text)
+                elif user_name == 'mirracles' and dice > 3 and len(text) > 10:
+                    self.writeIn(chat_id,text)
+                return None
---
+++ server.py
@@ -0,0 +1,36 @@
+import requests
+from mirrabot import mirrabot
+from requests.exceptions import HTTPError
+
+if __name__ == "__main__":
+    botBoi = mirrabot()
+    offset = 0
+    inFile = open('botKey.txt', 'r')
+    key = inFile.readline()
+    admin = inFile.readline()
+    mirra = inFile.readline()
+    inFile.close()
+    baseURL = 'https://api.telegram.org/bot'
+    baseURL += key + '/'
+    sendURL = baseURL + "sendMessage"
+    print("I'm on!")
+    while 1:
+        try:
+            result = requests.post(url = baseURL + 'getUpdates', data={"offset": offset})
+            result.raise_for_status()
+            msgList = result.json()["result"]
+            if len(msgList) > 0:
+                offset = msgList[-1]["update_id"] + 1
+                for msg in msgList:
+                    print(msg)
+                    result = botBoi.handleMe(msg['message'], admin, mirra)
+                    if result:
+                        try:
+                            requests.post(url = sendURL, params = result)
+                        except HTTPError as http_err:
+                            print("Send broke")
+        except HTTPError as http_err:
+            print("Update broke")
+        except KeyboardInterrupt as e:
+            print("End")
+            break
